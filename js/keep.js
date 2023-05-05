8f56ea5d3a3277750fb0284c6e7bbfb4b016a300
daltonya <96973600+TigerAsano@users.noreply.github.com>
2023-05-05 20:38:36 +0900

    Create keep.js

---
+++ js/keep.js
@@ -0,0 +1,50 @@
+document.addEventListener("keydown",e => {
+
+    if(!e.altKey) return;
+    if(!e.ctrlKey) return;
+    if(e.key !== "s") return;
+
+
+    const Aff = document.getElementById("Aff").outerHTML;
+    const Neg = document.getElementById("Neg").outerHTML;
+    const head = document.getElementsByClassName("head")[0].outerHTML;
+
+    const flowData = {
+        Aff,Neg,head
+    };
+
+    localStorage.setItem(window.location.href,JSON.stringify(flowData));
+
+});
+
+document.addEventListener("keydown",e => {
+
+    if(!e.altKey) return;
+    if(!e.ctrlKey) return;
+    if(e.key !== "d") return;
+
+    localStorage.removeItem(window.location.href);
+
+})
+
+
+function setEditedData(str){
+
+    const obj = JSON.parse(str);
+
+    for(const [key,html] of Object.entries(obj)){
+
+        if(key === "head") {
+
+            document.getElementsByClassName("head")[0].outerHTML = html;
+
+            continue;
+
+        }
+
+        
+        document.getElementById(key).outerHTML = html;
+
+    }
+
+}
