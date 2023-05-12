3ce25bc754169dae11447f21cc970fbaaf91d0d4
daltonya <96973600+TigerAsano@users.noreply.github.com>
2023-05-12 21:23:08 +0900

    Update sheet.js

--- js/sheet.js
+++ js/sheet.js
@@ -1,23 +1,18 @@
-function onChangeSheet() {
+function onChangeSheet(e){
 
-        const { value } = document.getElementById("flow");
+    const { value } = document.getElementById("flow");
 
-        if(value === "Neg") {
-            document.getElementById("Aff").style.display = "none";
-            document.getElementById("Neg").style.display = "block";
-            document.getElementById("Neg").style.display = "";
+    if(value === "Neg") {
+        document.getElementById("Aff").style.display = "none";
+        document.getElementById("Neg").style.display = "block";
+        document.getElementById("Neg").style.display = "";
 
+    
+    }else{
+        document.getElementById("Neg").style.display = "none";
+        document.getElementById("Aff").style.display = "blcok";
+        document.getElementById("Aff").style.display = "";
+        
+    }
 
-        }else{
-            document.getElementById("Neg").style.display = "none";
-            document.getElementById("Aff").style.display = "blcok";
-            document.getElementById("Aff").style.display = "";
-
-        }
-
-};
-
-document.getElementById("flow").addEventListener("change", onChangeSheet) ;
-
-console.log("hoge");
-
+}
